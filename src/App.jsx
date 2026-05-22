c8193f4c1921632e8b116fa75119adb2bf94fea6
AdiSalL <adiefsal@gmail.com>
2026-05-22 19:54:32 +0700

    feat:

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import ProductCard from "./ProductCard";
+import BookCard from "./BookCard";
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -10,8 +10,11 @@ function App() {
   async function fetchData() {
     setIsLoading(true);
     try {
-      const response = await axios.get("https://fakestoreapi.com/products");
-      setDataStore(response.data);
+      const response = await axios.get(
+        "https://openlibrary.org/search.json?q=programming&limit=10&language=eng",
+      );
+
+      setDataStore(response.data.docs);
     } catch (err) {
       setError("Gagal memuat produk. Coba lagi.");
       console.error(err);
@@ -27,10 +30,10 @@ function App() {
   if (isLoading)
     return (
       <div className="min-h-screen flex items-center justify-center">
-        <div class="flex flex-row gap-2">
-          <div class="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:.7s]"></div>
-          <div class="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:.3s]"></div>
-          <div class="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:.7s]"></div>
+        <div className="flex flex-row gap-2">
+          <div className="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:.7s]"></div>
+          <div className="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:.3s]"></div>
+          <div className="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:.7s]"></div>
         </div>
       </div>
     );
@@ -44,11 +47,11 @@ function App() {
 
   return (
     <div className="min-h-screen bg-gray-50 p-8">
-      <h1 className="text-2xl font-semibold text-gray-900 mb-6">Products</h1>
+      <h1 className="text-2xl font-semibold text-gray-900 mb-6">Books</h1>
       {/* fix: grid = row layout */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {dataStore.map((value) => (
-          <ProductCard key={value.id} value={value} />
+        {dataStore.map((book, index) => (
+          <BookCard key={book.key || index} book={book} />
         ))}
       </div>
     </div>
